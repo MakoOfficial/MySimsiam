43c33c517b63771bf58e1dceeaaab7bdaf66e7b0
Mako <759506695@qq.com>
2024-05-06 16:55:17 +0800

    modify linear_eval.py use Adam

--- linear_eval.py
+++ linear_eval.py
@@ -8,12 +8,12 @@ from arguments import get_args
 from augmentations import get_aug
 from models import get_model, get_backbone
 from tools import AverageMeter
-# from datasets import get_dataset
-from datasets.__init__Bone import get_dataset, get_single
+from datasets.__init__Bone import get_single
 from optimizers import get_optimizer, LR_Scheduler
+from torch.optim.lr_scheduler import StepLR
 
 
-def main(args):
+def main(args, lr=None):
     train_loader = torch.utils.data.DataLoader(
         dataset=get_single(
             data_dir=args.data_dir,
@@ -34,7 +34,12 @@ def main(args):
     )
 
     model = get_backbone(args.model.backbone)
-    classifier = nn.Linear(in_features=model.output_dim, out_features=228, bias=True).to(args.device)
+    classifier = nn.Sequential(
+        nn.Linear(in_features=model.output_dim, out_features=1024, bias=True),
+        nn.ReLU(),
+        nn.Linear(in_features=1024, out_features=228, bias=True)
+    )
+
     print(f"classifier input dim is {model.output_dim}, output dim is 228")
     assert args.eval_from is not None
     save_dict = torch.load(args.eval_from, map_location='cpu')
@@ -48,28 +53,28 @@ def main(args):
     # if torch.cuda.device_count() > 1: classifier = torch.nn.SyncBatchNorm.convert_sync_batchnorm(classifier)
     classifier = torch.nn.DataParallel(classifier)
     # define optimizer
-    optimizer = get_optimizer(
-        args.eval.optimizer.name, classifier,
-        lr=args.eval.base_lr * args.eval.batch_size / 256,
-        momentum=args.eval.optimizer.momentum,
-        weight_decay=args.eval.optimizer.weight_decay)
+    # optimizer = get_optimizer(
+    #     args.eval.optimizer.name, classifier,
+    #     lr=args.eval.base_lr * args.eval.batch_size / 256,
+    #     momentum=args.eval.optimizer.momentum,
+    #     weight_decay=args.eval.optimizer.weight_decay)
+    optimizer = torch.optim.Adam(classifier.parameters(), lr=lr, weight_decay=wd)
 
     # define lr scheduler
-    lr_scheduler = LR_Scheduler(
-        optimizer,
-        args.eval.warmup_epochs, args.eval.warmup_lr * args.eval.batch_size / 256,
-        args.eval.num_epochs, args.eval.base_lr * args.eval.batch_size / 256,
-                                 args.eval.final_lr * args.eval.batch_size / 256,
-        len(train_loader),
-    )
+    # lr_scheduler = LR_Scheduler(
+    #     optimizer,
+    #     args.eval.warmup_epochs, args.eval.warmup_lr * args.eval.batch_size / 256,
+    #     args.eval.num_epochs, args.eval.base_lr * args.eval.batch_size / 256,
+    #                              args.eval.final_lr * args.eval.batch_size / 256,
+    #     len(train_loader),
+    # )
+    lr_scheduler = StepLR(optimizer, step_size=10, gamma=0.5)
 
-    loss_meter = AverageMeter(name='Loss')
-    acc_meter = AverageMeter(name='Accuracy')
     loss_fn = nn.CrossEntropyLoss(reduction='sum')
     # Start training
     global_progress = tqdm(range(0, args.eval.num_epochs), desc=f'Evaluating')
+    best_loss = float('inf')
     for epoch in global_progress:
-        loss_meter.reset()
         model.eval()
         classifier.train()
         local_progress = tqdm(train_loader, desc=f'Epoch {epoch}/{args.eval.num_epochs}', disable=True)
@@ -78,42 +83,50 @@ def main(args):
         training_loss = 0.
 
         for idx, (images, labels) in enumerate(local_progress):
-            classifier.zero_grad()
+            # classifier.zero_grad()
+            optimizer.zero_grad()
             with torch.no_grad():
                 feature = model(images.to(args.device))
 
             labels = (labels-1).type(torch.LongTensor).to(args.device).squeeze()
+            print(f"labels.shape is {labels.shape}")
             preds = classifier(feature)
             preds = preds.squeeze()
-
+            print(f"preds.shape is {preds.shape}")
             loss = loss_fn(preds, labels)
 
             loss.backward()
             optimizer.step()
-            loss_meter.update(loss.item(), n=feature.shape[0])
-            lr = lr_scheduler.step()
 
             training_loss += loss.item()
             total_size += feature.shape[0]
 
-            local_progress.set_postfix({'lr': lr, 'loss': loss_meter.val, 'loss_avg': loss_meter.avg})
         train_avg = training_loss / total_size
-        print(f"epoch {epoch+1}, avg_loss: {train_avg}")
 
-    classifier.eval()
-    valid_loss, total = 0., 0.
-    acc_meter.reset()
-    for idx, (images, labels) in enumerate(test_loader):
-        with torch.no_grad():
-            feature = model(images.to(args.device))
-            preds = classifier(feature).argmax(dim=1)
-            labels = (labels-1).type(torch.LongTensor)
-            MAE_loss = F.l1_loss(preds, labels, reduction="sum").item()
-            valid_loss += MAE_loss
-            total += feature.shape[0]
-            acc_meter.update(val=MAE_loss, n=feature.shape[0])
-    print(f'Accuracy = {acc_meter.avg:.2f}, avg valid loss :{(valid_loss / total):.2f}')
+        classifier.eval()
+        valid_loss, total = 0., 0.
+        for idx, (images, labels) in enumerate(test_loader):
+            with torch.no_grad():
+                feature = model(images.to(args.device))
+                output = classifier(feature)
+                preds = torch.argmax(output, dim=1) + 1
+                labels = labels.to(args.device)
+
+                preds = preds.squeeze()
+                labels = labels.squeeze()
+
+                MAE_loss = F.l1_loss(preds, labels, reduction="sum").item()
+                valid_loss += MAE_loss
+                total += feature.shape[0]
+        valid_avg = valid_loss / total
+        print(f"epoch {epoch + 1}, train avg_loss: {train_avg}, valid avg_loss: {valid_avg}")
+        if valid_avg < best_loss:
+            best_loss = valid_loss
+        lr_scheduler.step()
+    print(f'best loss: {best_loss}')
 
 
 if __name__ == "__main__":
-    main(args=get_args())
+    lr = 5e-4
+    wd = 0
+    main(args=get_args(), lr=lr)
